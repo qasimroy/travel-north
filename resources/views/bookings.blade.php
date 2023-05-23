8ff66b2d322060fb2bfb9a3113908acc74cc3884
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-05-23 17:11:45 +0500

    day 2 2nd commit

--- resources/views/bookings.blade.php
+++ resources/views/bookings.blade.php
@@ -16,7 +16,61 @@
                             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                         </div>
                         <div class="offcanvas-body">
-                            ...
+                            <form class="row g-3 text-dark">
+                                <div class="col-6">
+                                  <label for="startDate" class="form-label">Start</label>
+                                  <input type="date" class="form-control" id="startDate" placeholder="1234 Main St">
+                                </div>
+                                <div class="col-6">
+                                  <label for="endDate" class="form-label">End</label>
+                                  <input type="date" class="form-control" id="endDate" placeholder="1234 Main St">
+                                </div>
+                                <div class="col-12">
+                                  <label for="inputAddress2" class="form-label">Address 2</label>
+                                  <input type="text" class="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor">
+                                </div>
+                                <div class="col-md-4">
+                                  <label for="inputCity" class="form-label">From</label>
+                                  <input type="text" class="form-control" id="inputCity">
+                                </div>
+                                <div class="col-md-4">
+                                  <label for="inputState" class="form-label">To</label>
+                                  <select id="inputState" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                  </select>
+                                </div>
+                                <div class="col-md-4">
+                                  <label for="inputZip" class="form-label">Passengers</label>
+                                  <input type="text" class="form-control" id="inputZip">
+                                </div>
+                                <div class="col-md-4">
+                                  <label for="inputCity" class="form-label">Hotel</label>
+                                  <input type="text" class="form-control" id="inputCity">
+                                </div>
+                                <div class="col-md-4">
+                                  <label for="inputState" class="form-label">Bike</label>
+                                  <select id="inputState" class="form-select">
+                                    <option selected>Choose...</option>
+                                    <option>...</option>
+                                  </select>
+                                </div>
+                                <div class="col-md-4">
+                                    <input type="checkbox" class="form-check-input s" id="inputZip">
+                                  <label for="inputZip" class="form-label">Car</label>
+                                </div>
+                                <div class="col-12">
+                                  <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                                    <label class="form-check-label" for="gridCheck">
+                                      Check me out
+                                    </label>
+                                  </div>
+                                </div>
+                                <div class="col-12">
+                                  <button type="submit" class="btn text-white" style="background: #67dcb1;">Book</button>
+                                </div>
+                              </form>
                         </div>
                         </div>
                         
