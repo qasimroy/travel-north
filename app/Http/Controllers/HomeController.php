283f7d6585e67cdb5a7eb7b0d16137a855834439
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-05-26 16:29:42 +0500

    day 5 commit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -26,8 +26,13 @@ class HomeController extends Controller
     public function index()
     {
         $serviceCount = Service::count();
-        $userCount = User::count();
-        $serviceProviderCount = User::where('id', 2)->count();
+        $userCount = User::whereHas('roles', function ($query) {
+            $query->where('name', 'User');
+        })->count();
+        $serviceProviderCount = User::whereHas('roles', function ($query) {
+            $query->where('name', 'Service Provider');
+        })->count();
+
         return view('home', compact('serviceCount', 'userCount', 'serviceProviderCount'));
     }
 }
