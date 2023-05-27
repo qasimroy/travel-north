dbf0124d1a8d38fdd85458a9e74e52d806f35943
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-05-27 18:53:38 +0500

    Day 6 commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -22,19 +22,5 @@ class ProfileController extends Controller
         } else {
             return redirect()->route('user.profile');
         }
-        // $adminRole = Role::where('name', 'Admin')->firstOrFail();
-        // $user = $adminRole->users()->first();
-
-        // return view('admin.profile', compact('user'));
-    }
-    public function update(Request $request)
-    {
-        $user = auth()->user();
-        $user->name = $request->input('name');
-        $user->email = $request->input('email');
-        $user->password = $request->input('password');
-        $user->save();
-
-        return redirect()->route('profile');
     }
 }
