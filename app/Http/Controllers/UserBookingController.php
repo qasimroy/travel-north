3dd42feefefece76f606aa40fe4e5b0a98c93aba
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-06-06 19:20:56 +0500

    day 10 commit

--- app/Http/Controllers/UserBookingController.php
+++ app/Http/Controllers/UserBookingController.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Booking;
+use Illuminate\Support\Facades\Auth;
 use App\Models\Service;
 use App\Models\User;
+use Illuminate\Http\Request;
 
 class UserBookingController extends Controller
 {
@@ -12,6 +15,52 @@ class UserBookingController extends Controller
         $services = Service::all();
         return view('user.bookings', compact('services'));
     }
+
+    public function store(Request $request)
+    {
+        // Validate the request data
+        $validatedData = $request->validate([
+            'startDate' => 'required|date',
+            'endDate' => 'required|date',
+            'service_id' => 'required|exists:services,id',
+            'service_provider_id' => 'required|exists:users,id',
+            'origin' => 'required|string',
+            'destination' => 'nullable|string',
+            'person' => 'required|integer',
+            'hotel' => 'nullable|string',
+            'coach' => 'nullable|string',
+            'shuttle' => 'nullable|string',
+            'price' => 'nullable|integer',
+            'status' => 'required|in:accepted,rejected,completed,pending',
+        ]);
+
+        // Fetch the current user ID
+        $user_id = Auth::id();
+
+        // Create a new booking instance and set its attributes
+        $booking = new Booking();
+        $booking->user_id = $user_id;
+        $booking->start_date = $validatedData['startDate'];
+        $booking->end_date = $validatedData['endDate'];
+        $booking->service_id = $validatedData['service_id'];
+        $booking->service_provider_id = $validatedData['service_provider_id'];
+        $booking->origin = $validatedData['origin'];
+        $booking->destination = $validatedData['destination'];
+        $booking->person = $validatedData['person'];
+        $booking->hotel = $validatedData['hotel'];
+        $booking->coach = $validatedData['coach'];
+        $booking->shuttle = $validatedData['shuttle'];
+        $booking->price = $validatedData['price'];
+        $booking->status = $validatedData['status'];
+        $booking->status = Booking::PENDING;
+
+        // Save the booking
+        $booking->save();
+
+        // Redirect to the bookings index page or show a success message
+        return redirect()->route('user.bookings')->with('success', 'Booking created successfully.');
+    }
+
 
     public function getServiceProviders(int $service_id)
     {
