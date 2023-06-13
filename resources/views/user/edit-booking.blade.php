87123cb5026026b18d230b69d9ae0511b5eeb271
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-06-13 19:53:01 +0500

    day 15 commit

--- resources/views/user/edit-booking.blade.php
+++ resources/views/user/edit-booking.blade.php
@@ -1,8 +1,8 @@
 @extends('layouts.app')
 @section('content')
-@include('service-provider.layouts.header')
-<h2 class="fs-2 m-0">Edit Booking</h2>
-</div>
+@include('user.layouts.header')
+        <h2 class="fs-2 m-0">Edit Booking</h2>
+    </div>
 </nav>
 <div class="container d-flex justify-content-center">
     <div class="w-50 p-5 bg-white rounded">
