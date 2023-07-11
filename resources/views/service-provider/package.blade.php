e7ec3889812ae25c304db830dac4ce766379e6fe
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-07-11 15:27:47 +0500

    feat: packages-show

--- resources/views/service-provider/package.blade.php
+++ resources/views/service-provider/package.blade.php
@@ -16,14 +16,25 @@
         <div class="row d-flex justify-content-around">
             @foreach ($packages as $package)
                 <div class="col-md-3">
-                    <div class="card" style="width: 18rem;">
+                    <div class="card shadow border-0 h-100" style="width: 18rem;">
                         <img src="{{ asset('storage/' . $package->image) }}" class="card-img-top image" alt="Package Image">
                         <div class="card-body">
-                            <h5 class="card-title">{{ $package->origin }} to {{ $package->destination }}</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of
-                                the
-                                card's content.</p>
-                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                            <h4 class="card-title">{{ $package->days }} Days trip from {{ $package->origin }} to
+                                {{ $package->destination }}</h4>
+                            <hr>
+                            <p class="card-text">{{ $package->days }} Days & {{ $package->days - 1 }} Nights <br>
+                                Rs. {{ $package->price }} <br>
+                                @if ($package->status == 'Open')
+                                    <h5 class="text-success">Open</h5>
+                                @elseif ($package->status == 'Closed')
+                                    <h5 class="text-danger">Closed</h5>
+                                @endif
+                            </p>
+                            <div class="d-flex align-items-end justify-content-between">
+                                <a href="#" class="btn btn-success">Explore <i class="fas fa-search"></i></a>
+                                <a href="#" class="btn btn-primary text-white">Edit <i class="fas fa-edit"></i></a>
+                                <a href="#" class="btn btn-danger ">Delete <i class="fas fa-trash"></i></a>
+                            </div>
                         </div>
                     </div>
                 </div>
