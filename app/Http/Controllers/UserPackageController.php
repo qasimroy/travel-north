d0238aed4ab64d9e1221d02244d57edcf8c6b261
M Qasim <71938192+qasimroy@users.noreply.github.com>
2023-07-25 16:52:42 +0500

    user package

--- app/Http/Controllers/UserPackageController.php
+++ app/Http/Controllers/UserPackageController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Package;
 use Illuminate\Http\Request;
 
 class UserPackageController extends Controller
 {
     public function index()
     {
-        return view('user.package');
+        $packages = Package::all();
+        return view('user.package', compact('packages'));
     }
 }
